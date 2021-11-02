8c087dc49c11658c0907d2d299d13219ebe9f10f
Noah <crashlive112@gmail.com>
2021-11-02 15:28:51 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+// (MDN) Callback function: function passed into another function as an action and later on invoked by the outer function in order to perform an action
 function addingEventListener() {
+  const input = document.getElementById('input');
+  // variable.addEventListener('event','callback function');
+  input.addEventListener('click', function () {
+    alert("This is an alert message!");
+  })
 }
